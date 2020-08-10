9d5a9ec4249af8508160c02174748ac623fd4aa9
DansCode <39342648+DansCode@users.noreply.github.com>
2020-08-10 14:47:09 -0400

    work completed

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,18 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+
+		String s = String.valueOf(num);
+		String[] a = s.split("(?!^)");
+		long len = s.length();
+
+		long total = 0;
+
+		for (String astring : a) {
+			total += Math.pow(Long.parseLong(astring), len);
+		}
+
+
+		return total == num;
 	}
 }
